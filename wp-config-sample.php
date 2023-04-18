2c2a6091e482b56d9a9ab664b899b4e82ccfaccb
Goke Pelemo <goke.pelemo@gmail.com>
2023-04-18 11:58:43 -0500

    update wp config and plugins

--- wp-config-sample.php
+++ wp-config-sample.php
@@ -1,96 +1,41 @@
 <?php
-/**
- * The base configuration for WordPress
- *
- * The wp-config.php creation script uses this file during the installation.
- * You don't have to use the web site, you can copy this file to "wp-config.php"
- * and fill in the values.
- *
- * This file contains the following configurations:
- *
- * * Database settings
- * * Secret keys
- * * Database table prefix
- * * ABSPATH
- *
- * @link https://wordpress.org/documentation/article/editing-wp-config-php/
- *
- * @package WordPress
- */
 
-// ** Database settings - You can get this info from your web host ** //
-/** The name of the database for WordPress */
-define( 'DB_NAME', 'database_name_here' );
+define( 'WP_HOME', getenv('SITEHOME') );
+define( 'WP_SITEURL', getenv('SITEURL') );
 
-/** Database username */
-define( 'DB_USER', 'username_here' );
+define( 'DB_NAME', getenv('MYSQL_DB') );
+define( 'DB_USER', getenv('MYSQL_USER') );
+define( 'DB_PASSWORD', getenv('MYSQL_PASS') );
+define( 'DB_HOST', getenv('MYSQL_HOST') );
+define( 'DB_CHARSET', getenv('MYSQL_CHARSET') );
+define( 'DB_COLLATE', getenv('MYSQL_COLLATE') );
+$table_prefix = getenv('MYSQL_PREFIX');
 
-/** Database password */
-define( 'DB_PASSWORD', 'password_here' );
+define( 'WP_CONTENT_DIR', getenv('CONTENTDIR') );
+define( 'WP_CONTENT_URL', getenv('CONTENTURL') );
 
-/** Database hostname */
-define( 'DB_HOST', 'localhost' );
+define( 'WP_PLUGIN_DIR', getenv('PLUGINDIR') );
+define( 'WP_PLUGIN_URL', getenv('PLUGINURL') );
 
-/** Database charset to use in creating database tables. */
-define( 'DB_CHARSET', 'utf8' );
+define( 'UPLOADS', getenv('UPLOADSDIR') );
 
-/** The database collate type. Don't change this if in doubt. */
-define( 'DB_COLLATE', '' );
+define( 'WP_ENVIRONMENT_TYPE', getenv('ENVIRONMENT') );
 
-/**#@+
- * Authentication unique keys and salts.
- *
- * Change these to different unique phrases! You can generate these using
- * the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}.
- *
- * You can change these at any point in time to invalidate all existing cookies.
- * This will force all users to have to log in again.
- *
- * @since 2.6.0
- */
-define( 'AUTH_KEY',         'put your unique phrase here' );
-define( 'SECURE_AUTH_KEY',  'put your unique phrase here' );
-define( 'LOGGED_IN_KEY',    'put your unique phrase here' );
-define( 'NONCE_KEY',        'put your unique phrase here' );
-define( 'AUTH_SALT',        'put your unique phrase here' );
-define( 'SECURE_AUTH_SALT', 'put your unique phrase here' );
-define( 'LOGGED_IN_SALT',   'put your unique phrase here' );
-define( 'NONCE_SALT',       'put your unique phrase here' );
+define( 'AUTH_KEY',         getenv('AUTH_KEY') );
+define( 'SECURE_AUTH_KEY',  getenv('SECURE_AUTH_KEY') );
+define( 'LOGGED_IN_KEY',    getenv('LOGGED_IN_KEY') );
+define( 'NONCE_KEY',        getenv('NONCE_KEY') );
+define( 'AUTH_SALT',        getenv('AUTH_SALT') );
+define( 'SECURE_AUTH_SALT', getenv('SECURE_AUTH_SALT') );
+define( 'LOGGED_IN_SALT',   getenv('LOGGED_IN_SALT') );
+define( 'NONCE_SALT',       getenv('NONCE_SALT') );
 
-/**#@-*/
-
-/**
- * WordPress database table prefix.
- *
- * You can have multiple installations in one database if you give each
- * a unique prefix. Only numbers, letters, and underscores please!
- */
-$table_prefix = 'wp_';
-
-/**
- * For developers: WordPress debugging mode.
- *
- * Change this to true to enable the display of notices during development.
- * It is strongly recommended that plugin and theme developers use WP_DEBUG
- * in their development environments.
- *
- * For information on other constants that can be used for debugging,
- * visit the documentation.
- *
- * @link https://wordpress.org/documentation/article/debugging-in-wordpress/
- */
 define( 'WP_DEBUG', false );
 
-/* Add any custom values between this line and the "stop editing" line. */
-
-
-
-/* That's all, stop editing! Happy publishing. */
-
-/** Absolute path to the WordPress directory. */
 if ( ! defined( 'ABSPATH' ) ) {
 	define( 'ABSPATH', __DIR__ . '/' );
 }
 
-/** Sets up WordPress vars and included files. */
+define( 'WP_MEMORY_LIMIT', getenv('MEMORYLIMIT') ?: '64M' )
+
 require_once ABSPATH . 'wp-settings.php';
